8bc4438197f78d2dc031735e23b3cd79cfbdaa54
aislannder <kenissom@hotmail.com>
2017-11-14 01:17:18 -0300

    Add files via upload

---
+++ CalculadoraSimples.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    public static class CalculadoraSimples
+    {
+
+        public static double Calcular(double valor1, double valor2, string operador)
+        {
+            double resultado = 0;
+
+            switch (operador)
+            {
+                case "÷":
+                    resultado = valor1 / valor2;
+                    break;
+                case "×":
+                    resultado = valor1 * valor2;
+                    break;
+                case "+":
+                    resultado = valor1 + valor2;
+                    break;
+                case "-":
+                    resultado = valor1 - valor2;
+                    break;
+                case "":
+                    resultado = valor1;
+                    break;
+  
+            }
+
+            return resultado;
+        }
+    }
+}
---
+++ SharedResources.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Calculadora
+{
+    public static class SharedResources
+    {
+        public static Color OpButtonBkColor
+        {
+            get
+            {
+                //return Color.White;
+                return Color.FromHex("#1abc9c");
+            }
+        }
+    }
+}
